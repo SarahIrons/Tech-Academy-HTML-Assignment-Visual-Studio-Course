76f3b5a385fcf70c1cf4e9ca166b3574223886e9
SarahIrons <sarahirons11@gmail.com>
2024-01-16 17:25:40 -0500

    Add project files.

---
+++ HTMLAssignment/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+var lineDrawing = anime({
+    targets: '#lineDrawing .lines path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 3000,
+    delay: function (el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+});
